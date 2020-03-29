0ce4c99c6f3918d256fb983cdf266704a9c66204
tuacy <1007178106@qq.com>
2020-03-29 15:34:13 +0800

    es(elasticsearch)学习

---
+++ collect/elasticsearch/src/main/java/com/tuacy/elasticsearch/ElasticsearchApplication.java
@@ -0,0 +1,18 @@
+package com.tuacy.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuyx
+ * @version 1.0
+ * @date 2020/3/29 13:00
+ */
+@SpringBootApplication()
+public class ElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class, args);
+    }
+
+}
