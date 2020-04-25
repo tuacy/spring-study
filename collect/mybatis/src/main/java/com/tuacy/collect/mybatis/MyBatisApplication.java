468345afddc47c8638d15ff20e6ac8730253a1ef
tuacy <1007178106@qq.com>
2020-04-25 13:45:45 +0800

    mybatis

---
+++ collect/mybatis/src/main/java/com/tuacy/collect/mybatis/MyBatisApplication.java
@@ -0,0 +1,18 @@
+package com.tuacy.collect.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuyx
+ * @version 1.0
+ * @date 2020/4/25 12:53
+ */
+@SpringBootApplication
+public class MyBatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisApplication.class, args);
+    }
+
+}
